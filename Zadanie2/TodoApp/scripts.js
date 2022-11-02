9db02305c1e17ff5c7f167941a4a035637c9a11c
matkrasinski <mat.krasinski@o2.pl>
2022-11-02 18:19:48 +0100

    Final version

--- Zadanie2/TodoApp/scripts.js
+++ Zadanie2/TodoApp/scripts.js
@@ -1,20 +1,22 @@
 "use strict"
 let todoList = []; //declares a new array for Your todo list
+const MASTER_KEY = '$2b$10$FxWaSLC9x2/A2qDK3nrrS.cSLoStAjfgOYnqGUzBW0XuUnErp.DmC';
+const BIN_URL = 'https://api.jsonbin.io/v3/b/63486a2f65b57a31e69552bc';
 
 let initList = function() {
   $.ajax({
     // copy Your bin identifier here. It can be obtained in the dashboard
-    url: 'https://api.jsonbin.io/v3/b/63486a2f65b57a31e69552bc',
+    url: BIN_URL,
     type: 'GET',
     headers: { //Required only if you are trying to access a private bin
-      'X-MASTER-KEY': '$2b$10$FxWaSLC9x2/A2qDK3nrrS.cSLoStAjfgOYnqGUzBW0XuUnErp.DmC',
+      'X-MASTER-KEY': MASTER_KEY
     },
     success: (data) => {
       todoList = data.record;
       if ($(document).ready(updateTodoList()));
     },
     error: (err) => {
-      console.log(err.responseJSON);
+      console.error(err.responseJSON);
     }
    });
 }
@@ -22,18 +24,15 @@ if ($(document).ready(initList()));
 
 let updateJSONbin = function() {
   $.ajax({
-    url: 'https://api.jsonbin.io/v3/b/63486a2f65b57a31e69552bc',
+    url: BIN_URL,
     type: 'PUT',
     headers: { //Required only if you are trying to access a private bin
-      'X-MASTER-KEY': '$2b$10$FxWaSLC9x2/A2qDK3nrrS.cSLoStAjfgOYnqGUzBW0XuUnErp.DmC',
+      'X-MASTER-KEY': MASTER_KEY
     },
     contentType: 'application/json',
     data: JSON.stringify(todoList),
-    success: (data) => {
-      console.log(data);
-    },
     error: (err) => {
-      console.log(err.responseJSON);
+      console.error(err.responseJSON);
     }
   });
 }
@@ -78,9 +77,7 @@ let updateTodoList = function() {
   let todoListDiv = $("#todoListView");
   todoListDiv.empty();
 
-  $("<tr><th class='title'>Title</th><th class='desc'>Description</th></tr>" 
-    
-  ).appendTo(todoListDiv);
+  $("<tr><th>Title</th><th>Description</th></tr>").appendTo(todoListDiv);
 
   let filterInput = $("#inputSearch");
   var endDate = convertToNumber(new Date($('#endDate').val()));
@@ -97,9 +94,7 @@ let updateTodoList = function() {
       thisDate == 19700101)         // null value is converted to this date 1970/01/01 
     ) {
       // declaring new row
-        let newRow = $("<tr></tr>", {
-          // class : "col py-3 px-lg-5 border bg-light"
-        });  
+        let newRow = $("<tr></tr>");
       // new title data
         $("<td></td>",{             
           text : todoList[todo].title,    
@@ -116,13 +111,11 @@ let updateTodoList = function() {
             type : "button",
             value : "Delete",
             id : "delete_btn",
-            class : "btn btn-outline-primary btn-sm btn-rounded",
-            
+            class : "btn btn-outline-primary btn-sm btn-rounded"
           }).click(function() {
             deleteTodo(todo);
             updateTodoList();
         }).appendTo(newRow);
-
       todoListDiv.append(newRow);
       }
     }
