4dc61042868ba4ac1f6eb0db5882f9910052f485
matkrasinski <mat.krasinski@o2.pl>
2022-11-19 17:16:29 +0100

    Search Engine, Displaying all data for now

---
+++ Zadanie3/zadanie3/src/components/MoviesTable.vue
@@ -0,0 +1,17 @@
+<template>
+    <div v-for="movie in movieList" :key="movie.ttle">
+        <p>{{movie.title}}, {{movie.year}}, {{movie.genres}}, {{movie.cast}}</p>
+    </div>
+</template>
+
+<script>
+import jsonData from '..//../data/movies.json'
+export default {
+    name: 'MoviesTable.vue',
+    data() {
+        return {
+            movieList: jsonData
+        }
+    }
+}
+</script>
---
+++ Zadanie3/zadanie3/src/components/SearchEngine.vue
@@ -0,0 +1,39 @@
+<template>
+    <header>
+        <h1> Movies </h1>
+    </header>
+    <div>
+        <div class="form-group">
+            <label for="movieTitle">Title : </label>
+            <input type="text" class="form-control" id="movieTitle" placeholder="Movie title"/>
+        </div>
+        <div class="form-group">
+            <label for="fromDate">From the year of production : </label>
+            <input type="text" class="form-control" id="fromDate" placeholder="Year from"/>
+        </div>
+        <div class="form-group">
+            <label for="tillDate">Until the year of production : </label>
+            <input type="text" class="form-control" id="tillDate" placeholder="Year till"/>
+        </div>
+        <div class="form-group">
+            <label for="actorName">Cast : </label>
+            <input type="text" class="form-control" id="actorName" placeholder="Actor">
+        </div>
+        <div>
+            <button class="btn btn-primary col-12" >Search</button>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'SearchEngine.vue'
+    }
+</script>
+
+<style>
+    label {
+        font-weight: bold;
+    }
+ 
+</style>
